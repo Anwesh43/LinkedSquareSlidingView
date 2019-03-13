37c808cf0f2dccd246d8330e02eb4c8bbc837472
Anwesh43 <anweshthecool0@gmail.com>
2019-03-13 23:55:34 +0530

    adding SquareSliding class

--- squareslidingview/src/main/java/com/anwesh/uiprojects/squareslidingview/SquareSlidingView.kt
+++ squareslidingview/src/main/java/com/anwesh/uiprojects/squareslidingview/SquareSlidingView.kt
@@ -163,4 +163,28 @@ class SquareSlidingView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareSliding(var i : Int) {
+
+        private var root : SSNode = SSNode(0)
+        private var curr : SSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
