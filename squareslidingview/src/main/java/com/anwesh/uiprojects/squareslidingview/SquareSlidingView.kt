c2dda24dcfbf98da6ada6ab6a042e5eaeac4bb1d
Anwesh43 <anweshthecool0@gmail.com>
2019-03-13 23:52:52 +0530

    adding SSNode class

--- squareslidingview/src/main/java/com/anwesh/uiprojects/squareslidingview/SquareSlidingView.kt
+++ squareslidingview/src/main/java/com/anwesh/uiprojects/squareslidingview/SquareSlidingView.kt
@@ -119,4 +119,48 @@ class SquareSlidingView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SSNode(var i : Int, val state : State = State()) {
+
+        private var next : SSNode? = null
+        private var prev : SSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SSNode {
+            var curr : SSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
