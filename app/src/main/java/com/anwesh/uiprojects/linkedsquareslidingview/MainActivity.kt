3080d78ceea731811beea6a26c1a2a4fecc86b97
Anwesh43 <anweshthecool0@gmail.com>
2019-03-14 00:00:51 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquareslidingview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquareslidingview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquareslidingview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squareslidingview.SquareSlidingView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareSlidingView.create(this)
     }
 }
