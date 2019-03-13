8a087987668ad60d1abc3e0ecf95cf85635072e8
Anwesh43 <anweshthecool0@gmail.com>
2019-03-13 12:02:35 +0530

    adding libraries and constants

--- squareslidingview/src/main/java/com/anwesh/uiprojects/squareslidingview/SquareSlidingView.kt
+++ squareslidingview/src/main/java/com/anwesh/uiprojects/squareslidingview/SquareSlidingView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.squareslidingview
 /**
  * Created by anweshmishra on 13/03/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val squares : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
